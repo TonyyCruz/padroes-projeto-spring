389008039fe7c8efc4c11705033f846af69fd47e
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-08 14:43:39 -0300

    create: interface IClientService

---
+++ src/main/java/anthony/cruz/padroesprojetospring/service/IClientService.java
@@ -0,0 +1,18 @@
+package anthony.cruz.padroesprojetospring.service;
+
+import anthony.cruz.padroesprojetospring.model.Client;
+
+import java.util.List;
+
+public interface IClientService {
+
+    List<Client> findAll();
+
+    Client findById(Long id);
+
+    void insert(Client client);
+
+    void update(Long id, Client client);
+
+    void delete(Long id);
+}
