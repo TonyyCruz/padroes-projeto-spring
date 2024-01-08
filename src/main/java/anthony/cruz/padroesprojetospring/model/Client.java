744ad34952041ccc46c196891522f1c35ae93ea8
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-08 14:43:13 -0300

    create: model Client

---
+++ src/main/java/anthony/cruz/padroesprojetospring/model/Client.java
@@ -0,0 +1,19 @@
+package anthony.cruz.padroesprojetospring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Address address;
+}
