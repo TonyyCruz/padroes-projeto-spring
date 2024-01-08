0515d170f43d6434f3772f5306cc736035bf3c9c
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-01-08 15:58:50 -0300

    feat: implements ClientController

--- src/main/java/anthony/cruz/padroesprojetospring/controller/ClientController.java
+++ src/main/java/anthony/cruz/padroesprojetospring/controller/ClientController.java
@@ -1,9 +1,51 @@
 package anthony.cruz.padroesprojetospring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import anthony.cruz.padroesprojetospring.model.Client;
+import anthony.cruz.padroesprojetospring.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> insert(@RequestBody Client client) {
+        Client newClient = service.insert(client);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newClient.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(newClient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client) {
+        Client updatedClient = service.update(id, client);
+        return ResponseEntity.ok(updatedClient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
